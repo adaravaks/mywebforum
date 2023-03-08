1e162f0f4c8fedb2676b919aad1f33496ebf287e
adaravaks <tseveleva2006@gmail.com>
2023-03-08 22:33:52 +0700

    changed and hided secret key cause i want repository to be public

--- Webforum/Webforum/settings.py
+++ Webforum/Webforum/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fbww$^d$a$&94^w@u$lsm@(wo2-ca=p4ltlu=4#s7tiij+5(%+'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -141,6 +142,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'tseveleva2006@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_FROM = 'noreply<no-reply@comfortplace.com>'
 PASSWORD_RESET_TIMEOUT = 7200
